ba82e35f68428e8834736af79f47de0323d79cdd
Stan <fernfiesta@gmail.com>
2016-11-09 09:59:11 -0800

    BSTIterator

---
+++ src/main/java/com/chawkalla/algorithms/examples/tree/BSTIterator.java
@@ -0,0 +1,73 @@
+package com.chawkalla.algorithms.examples.tree;
+
+import java.util.Stack;
+
+public class BSTIterator {
+	Stack<TreeNode> st;
+	TreeNode cur;
+	
+	public BSTIterator(TreeNode root) {
+		st=new Stack<TreeNode>();
+		cur=root;			
+	}
+
+	/** @return whether we have a next smallest number */
+	public boolean hasNext() {
+		return (cur!=null || !st.isEmpty());
+	}
+
+	/** @return the next smallest number */
+	public int next() {
+		int val=0;
+
+		boolean done=false;		
+		while(!done){
+			if(cur!=null){
+				st.push(cur);
+				cur=cur.left;
+			}else if(!st.isEmpty()){
+				cur=st.pop();
+				val=cur.val;
+				cur=cur.right;
+				done=true;
+			}else
+				done=true; //done when stack empty here
+		}
+		return val;
+	}
+	public static void main(String[] args) {
+		TreeNode root=new TreeNode(10);
+		TreeNode node5=new TreeNode(5);
+		TreeNode node2=new TreeNode(2);
+		TreeNode node7=new TreeNode(7);
+		TreeNode node15=new TreeNode(15);
+		TreeNode node12=new TreeNode(12);
+		TreeNode node18=new TreeNode(18);
+		/*
+		 * 					10		
+		 * 			5				15
+		 * 		2		7		12		18
+		 * 
+		 * 
+		 * 
+		 */
+		
+		root.left=node5;
+		root.right=node15;
+		node5.left=node2;
+		node5.right=node7;
+		node15.left=node12;
+		node15.right=node18;
+
+		BSTIterator iter=new BSTIterator(root);
+		while(iter.hasNext())
+			System.out.println(iter.next());
+	}
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+
+}
